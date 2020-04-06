8ed2e27d61057e7856b407608873fbf0b60cb400
paulor8bit <thunder_momon@hotmail.com>
2020-04-06 15:31:30 -0300

    vue monty

--- vue/monty/src/main.js
+++ vue/monty/src/main.js
@@ -1,8 +1,7 @@
 import Vue from 'vue'
-import App from './App.vue'
+import App from './App'
 
-Vue.config.productionTip = false
-
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
+new Vue ({ 
+    el: '#app',
+    render: aplica => aplica(App)
+})
