d87b14e686355165a85b3d80c03a30091be253ef
paulor8bit <thunder_momon@hotmail.com>
2020-04-08 20:24:57 -0300

    react

---
+++ react/calculadorareact/build/precache-manifest.afdb6c7af2d276195e9e52e073f6aedf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4309cc6560d2b530e33565bb6eb7a336",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0074a14ffdc56ecec473",
+    "url": "/static/css/main.7aca62f6.chunk.css"
+  },
+  {
+    "revision": "706d1ce96973a6c2e5c2",
+    "url": "/static/js/2.aa4de7f8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.aa4de7f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0074a14ffdc56ecec473",
+    "url": "/static/js/main.f5849168.chunk.js"
+  },
+  {
+    "revision": "d2f2fe1ae1130c794d7b",
+    "url": "/static/js/runtime-main.ad5ec763.js"
+  },
+  {
+    "revision": "b93c2ad436ccb747ae415c1e865819a4",
+    "url": "/static/media/RobotoMono-Thin.b93c2ad4.ttf"
+  }
+]);
