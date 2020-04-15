35104aeae00b041ae16b1e5ac6af2af946ba776f
paulor8bit <thunder_momon@hotmail.com>
2020-04-14 21:53:37 -0300

    Bootstrap

---
+++ bootstrap/exercicios/js/navegacao.js
@@ -0,0 +1,39 @@
+(function () {
+    function navegarViaAjax(hash) {
+        if (!hash) return
+
+        const link = document.querySelector(`[wm-link='${hash}']`)
+        if(!link) return
+
+        const destino = document.querySelector('[wm-link-destino]')
+
+        const url = hash.substring(1)
+        fetch(url)
+            .then(resp => resp.text())
+            .then(html => {
+                destino.innerHTML = html
+                eval(html.macth(/\<script\>([\s\S]*)\<\/script\>/)[1])
+            })
+    }
+
+    function configurarLinks() {
+        document.querySelectorAll('[wm-link]')
+            .forEach(link => {
+                link.href = link.attributes['wm-link'].value
+            })
+    }
+
+    function navegacaoInicial() {
+        if (location.hash) {
+            navegarViaAjax(location.hash)
+        } else {
+            const primeiroLink = document.querySelector('[wm-link]')
+            navegarViaAjax(primeiroLink.hash)
+        }
+    }
+
+    window.onhashchange = e => navegarViaAjax(location.hash)
+    
+    configurarLinks()
+    navegacaoInicial()
+})()
