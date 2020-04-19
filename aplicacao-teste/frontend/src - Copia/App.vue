8063db14be6ddc56d732047296b2841a9fd62a6f
paulor8bit <thunder_momon@hotmail.com>
2020-04-19 17:18:16 -0300

    Aplicação Teste

---
+++ aplicacao-teste/frontend/src - Copia/App.vue
@@ -0,0 +1,61 @@
+<template>
+	<div id="app" :class="{'hide-menu': !isMenuVisible || user}">
+		<Header title="Base de Conhecimento" 
+		:hideToggle="!user"
+		:hideUserDropdown="!user"/>
+		<Menu v-if="user" />
+		<Auth />
+		<Content  />
+		<Footer />
+		
+	</div>
+</template>
+
+<script>
+// O @ faz ir da pasta src
+import {mapState} from 'vuex'
+import Header from "@/components/template/Header"
+import Menu from "@/components/template/Menu"
+import Content from "@/components/template/Content"
+import Footer from "@/components/template/Footer"
+import Auth from  "@/components/auth/Auth"
+
+export default {
+	name: "App",
+	components: { Header, Menu, Content, Footer},
+	computed: mapState(['isMenuVisible', 'user'])
+}
+</script>
+
+<style>
+	* { 
+		font-family: "Lato", sans-serif;
+	}
+
+	body { 
+		margin: 0;
+	}
+
+	#app { 
+		-webkit-font-soothing: antialiased; 
+		-moz-osx-font-smoothing: grayscale; 
+
+		height: 100vh;
+
+		display: grid;
+		grid-template-rows: 60px 1fr 40px;
+		grid-auto-columns: 300px 1fr;
+		grid-template-areas: 
+		"header header"
+		"menu content"
+		"menu footer"
+	}
+
+	#app.hide-menu { 
+		grid-template-areas: 
+		"header header"
+		"content content"
+		"footer footer"
+	}	
+	
+</style>
