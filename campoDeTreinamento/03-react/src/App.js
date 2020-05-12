b7e2b509c0aa176d45278975bf4201e5912c33e2
paulor8bit <thunder_momon@hotmail.com>
2020-05-12 13:46:32 -0300

    Estado Imutabilidade

--- campoDeTreinamento/03-react/src/App.js
+++ campoDeTreinamento/03-react/src/App.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import './App.css';
+import TechList from './components/TechList'
 
 function App () { 
-    return <h1>Hello Paulinho</h1>
+    return <TechList />
 }
 
 export default App;
