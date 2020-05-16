3e19739012426388019c56f7713daa8dc02c9e9a
paulor8bit <thunder_momon@hotmail.com>
2020-05-15 21:56:26 -0300

    eslint

--- campoDeTreinamento/primeiroprojetonat/App.js
+++ campoDeTreinamento/primeiroprojetonat/App.js
@@ -1,25 +1,24 @@
-import React, {Component} from 'react';
-import {StyleSheet, View, Text} from 'react-native';
+import React from 'react';
+import { StyleSheet, View, Text } from 'react-native';
 
-export default class App extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Bem vindo ao React</Text>
-      </View>
-    );
-  }
-}
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
+    welcome: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
+    },
 });
+
+export default function App() {
+    return (
+        <View style={styles.container}>
+            <Text style={styles.welcome}>Bem vindo ao React</Text>
+        </View>
+    );
+}
