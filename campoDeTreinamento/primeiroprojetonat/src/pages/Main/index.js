427f9f97915d283409e1ed23122fa01a31b16580
paulor8bit <thunder_momon@hotmail.com>
2020-05-17 15:51:40 -0300

    Projeto react native

--- campoDeTreinamento/primeiroprojetonat/src/pages/Main/index.js
+++ campoDeTreinamento/primeiroprojetonat/src/pages/Main/index.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import { Keyboard, ActivityIndicator } from 'react-native';
+import AsyncStorage from '@react-native-community/async-storage';
+import PropTypes from 'prop-types';
 import {
     Container,
     Form,
@@ -18,12 +20,28 @@ import api from '../../services/api';
 
 Icon.loadFont();
 
+const KEY_ASYNC_STORAGE = '@intro-rn:user:key';
+
 class Main extends Component {
     state = {
         newUser: '',
         users: [],
         loading: false,
     };
+
+    async componentDidMount() {
+        const users = await AsyncStorage.getItem(KEY_ASYNC_STORAGE);
+        if (users) {
+            this.setState({ users: JSON.parse(users) });
+        }
+    }
+
+    componentDidUpdate(_, prevState) {
+        const { users } = this.state;
+        if (prevState.users !== users) {
+            AsyncStorage.setItem(KEY_ASYNC_STORAGE, JSON.stringify(users));
+        }
+    }
 
     handleAddUser = async () => {
         const { users, newUser } = this.state;
@@ -40,6 +58,11 @@ class Main extends Component {
         this.setState({ users: [...users, data], newUser: '', loading: false });
 
         Keyboard.dismiss();
+    };
+
+    handleNavigate = (user) => {
+        const { navigation } = this.props;
+        navigation.navigate('User', { user });
     };
 
     render() {
@@ -76,7 +99,9 @@ class Main extends Component {
                             <Avatar source={{ uri: item.avatar }} />
                             <Name>{item.name}</Name>
                             <Bio>{item.bio}</Bio>
-                            <ProfileButton onPress={() => {}}>
+                            <ProfileButton
+                                onPress={() => this.handleNavigate(item)}
+                            >
                                 <ProfileButtonText>
                                     Ver perfil
                                 </ProfileButtonText>
@@ -91,6 +116,12 @@ class Main extends Component {
 
 Main.navigationOptions = {
     title: 'Usuários',
+};
+
+Main.propTypes = {
+    navigation: PropTypes.shape({
+        navigate: PropTypes.func,
+    }).isRequired,
 };
 
 export default Main;
--- campoDeTreinamento/primeiroprojetonat/src/pages/User/index.js
+++ campoDeTreinamento/primeiroprojetonat/src/pages/User/index.js
@@ -1,10 +1,72 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import {
+    Container,
+    Header,
+    Avatar,
+    Name,
+    Bio,
+    Stars,
+    Starred,
+    OwnerAvatar,
+    Info,
+    Author,
+    Title,
+} from './styles';
+import api from '../../services/api';
 
-// import { Container } from './styles';
+export default class User extends Component {
+    static navigationOptions = ({ navigation }) => ({
+        title: navigation.getParam('user').name,
+    });
 
-const Main = () => {
-    return <View />;
+    state = {
+        stars: [],
+    };
+
+    async componentDidMount() {
+        const { navigation } = this.props;
+        const user = navigation.getParam('user');
+        const response = await api.get(`/users/${user.login}/starred`);
+
+        this.setState({ stars: response.data });
+    }
+
+    render() {
+        const { stars } = this.state;
+        const { navigation } = this.props;
+        const user = navigation.getParam('user');
+
+        return (
+            <Container>
+                <Header>
+                    <Avatar source={{ uri: user.avatar }} />
+                    <Name>{user.name}</Name>
+                    <Bio>{user.bio}</Bio>
+                </Header>
+
+                <Stars
+                    data={stars}
+                    keyExtractor={(star) => String(star.id)}
+                    renderItem={({ item }) => (
+                        <Starred>
+                            <OwnerAvatar
+                                source={{ uri: item.owner.avatar_url }}
+                            />
+                            <Info>
+                                <Title>{item.name}</Title>
+                                <Author>{item.owner.login}</Author>
+                            </Info>
+                        </Starred>
+                    )}
+                />
+            </Container>
+        );
+    }
+}
+
+User.propTypes = {
+    navigation: PropTypes.shape({
+        getParam: PropTypes.func,
+    }).isRequired,
 };
-
-export default Main;
