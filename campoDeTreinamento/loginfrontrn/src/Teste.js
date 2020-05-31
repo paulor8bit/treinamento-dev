eec18260eee253e55cf2b4ed4e0007734a516036
paulor8bit <thunder_momon@hotmail.com>
2020-05-31 12:02:05 -0300

    Projeto React Native Frontend

---
+++ campoDeTreinamento/loginfrontrn/src/Teste.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+// import { Container } from './styles';
+
+const src = () => {
+  return <Text>TEste</Text>;
+};
+
+export default src;
