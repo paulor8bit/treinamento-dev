52dab6095461dbaea326ecabacd46346eba876b1
paulor8bit <thunder_momon@hotmail.com>
2020-06-13 16:49:18 -0300

    Aula 47, 48, 49 - Campo Minado Projeto

---
+++ react-Native/campoMinado/App.js
@@ -0,0 +1,37 @@
+ 
+import React, { Component } from 'react';
+import { StyleSheet, View, Text} from 'react-native';
+import params from './src/params'
+import Field from './src/components/Field'
+
+export default class App extends Component {
+  render(){
+    return (
+      <View style={styles.container}>
+          <Text style={styles.welcome}>Iniciando campo Minado</Text>
+          <Text style={styles.instructions}>Tamanho da grade: {params.getRowsAmount()}x{params.getColumnsAmount()}
+          </Text>
+          <Field />
+      </View>
+    )
+  }
+}
+ 
+const styles = StyleSheet.create({
+  container:{
+    flex: 1,
+    justifyContent: 'center',
+    alignItems:'center',
+    backgroundColor: '#F5FCFF'
+  },
+  welcome:{
+    fontSize: 20,
+    textAlign: 'center',
+    margin:10,
+  },
+  instructions:{
+    fontSize: 15,
+    textAlign: 'center',
+    margin:10,
+  }
+})
