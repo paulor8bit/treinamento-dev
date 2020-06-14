6818c7aaf883880349b0f769bfcdbd54c7258377
paulor8bit <thunder_momon@hotmail.com>
2020-06-14 17:53:01 -0300

    56. Componente Tabuleiro (MineField)

--- react-Native/campoMinado/App.js
+++ react-Native/campoMinado/App.js
@@ -2,21 +2,38 @@
 import React, { Component } from 'react';
 import { StyleSheet, View, Text} from 'react-native';
 import params from './src/params'
-import Field from './src/components/Field'
 
+import MineField from './src/components/MineField'
+import {createMinedBoard} from './src/functions'
 export default class App extends Component {
+ 
+ constructor(props) { 
+   super(props)
+   this.state = this.createState()
+ }
+ 
+  minesAmount = () => {
+    const cols = params.getColumnsAmount()
+    const rows = params.getRowsAmount()
+    return Math.ceil(cols * rows * params.difficultLevel)
+  }
+
+  createState = () => { 
+    const cols = params.getColumnsAmount()
+    const rows = params.getRowsAmount()
+    return { 
+      board: createMinedBoard(rows, cols, this.minesAmount()),
+    }
+  }
+
+
   render(){
     return (
       <View style={styles.container}>
-          <Text style={styles.welcome}>Iniciando campo Minado</Text>
-          <Text style={styles.instructions}>Tamanho da grade: {params.getRowsAmount()}x{params.getColumnsAmount()}
-          </Text>
-          <Field />
-          <Field opened nearMines={3}/>
-          <Field mined/>
-          <Field mined opened/>
-          <Field mined opened exploded/>
-          <Field flagged/>
+
+              <View style={styles.board}>
+                <MineField board={this.state.board} />
+              </View> 
       </View>
     )
   }
@@ -25,18 +42,12 @@ export default class App extends Component {
 const styles = StyleSheet.create({
   container:{
     flex: 1,
-    justifyContent: 'center',
+    justifyContent: 'flex-end',
     alignItems:'center',
     backgroundColor: '#F5FCFF'
   },
-  welcome:{
-    fontSize: 20,
-    textAlign: 'center',
-    margin:10,
-  },
-  instructions:{
-    fontSize: 15,
-    textAlign: 'center',
-    margin:10,
+  board: {
+    alignItems: 'center', 
+    backgroundColor: '#AAA'
   }
 })
--- react-Native/campoMinado/src/functions.js
+++ react-Native/campoMinado/src/functions.js
@@ -1,4 +1,4 @@
-const createBoard = (row, columns) => {
+const createBoard = (rows, columns) => {
     return Array(rows).fill(0).map((_, row) => {
         return Array(columns).fill(0).map((_, column) => {
             return {
@@ -14,7 +14,7 @@ const createBoard = (row, columns) => {
 }
 
 const spreadMines = (board, minesAmount) => { 
-    const row = board.length
+    const rows = board.length
     const columns = board[0].length
     let minesPlanted=0
     while (minesPlanted < minesAmount) { 
@@ -34,4 +34,4 @@ const createMinedBoard = (rows, columns, minesAmount) => {
     return board
 }
 
-export {createBoard}
+export {createMinedBoard}
