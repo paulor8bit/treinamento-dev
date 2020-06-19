9897dedaaab321ac355502bc5e991eeedf04e080
paulor8bit <thunder_momon@hotmail.com>
2020-06-18 22:34:33 -0300

    78. Adicionar FlatList

--- react-Native/tasksOffline/src/screens/TaskList.js
+++ react-Native/tasksOffline/src/screens/TaskList.js
@@ -1,79 +1,87 @@
-import React, {Component} from 'react'
-import {View, Text, ImageBackground, StyleSheet} from 'react-native'
+import React, { Component } from 'react'
+import { View, Text, ImageBackground, StyleSheet, FlatList } from 'react-native'
+
+import commonStyles from '../commonStyles'
 import todayImage from '../../assets/imgs/today.jpg'
+
 import moment from 'moment'
 import 'moment/locale/pt-br'
-import commonStyles from '../commonStyles'
+
 import Task from '../components/Task'
+
 export default class TaskList extends Component {
+    state = {
+        tasks: [{
+            id: Math.random(),
+            desc: 'Comprar Livro de React Native',
+            estimateAt: new Date(),
+            doneAt: new Date(),
+        }, {
+            id: Math.random(),
+            desc: 'Ler Livro de React Native',
+            estimateAt: new Date(),
+            doneAt: null,
+        }]
+    }
+
+    toggleTask = taskId => {
+        const tasks = [...this.state.tasks]
+        tasks.forEach(task => {
+            if(task.id === taskId) {
+                task.doneAt = task.doneAt ? null : new Date()
+            }
+        })
+
+        this.setState({ tasks })
+    }
+
     render() {
         const today = moment().locale('pt-br').format('ddd, D [de] MMMM')
         return (
-            
             <View style={styles.container}>
                 <ImageBackground source={todayImage}
-                style={styles.background}>
+                    style={styles.background}>
                     <View style={styles.titleBar}>
                         <Text style={styles.title}>Hoje</Text>
                         <Text style={styles.subtitle}>{today}</Text>
                     </View>
-
-
                 </ImageBackground>
-                
                 <View style={styles.taskList}>
-
-                <Task desc="Comprar Livro" estimateAt={new Date()}
-                doneAt={new Date()}/>
-                <Task desc="Ler Livros" estimateAt={new Date()}
-                doneAt={null}/>
-                <Task desc="Comprar Livro" estimateAt={new Date()}
-                doneAt={new Date()}/>
-                <Task desc="Ler Livros" estimateAt={new Date()}
-                doneAt={null}/>
-                <Task desc="Comprar Livro" estimateAt={new Date()}
-                doneAt={new Date()}/>
-                <Task desc="Ler Livros" estimateAt={new Date()}
-                doneAt={null}/>
-                <Task desc="Comprar Livro" estimateAt={new Date()}
-                doneAt={new Date()}/>
-                <Task desc="Ler Livros" estimateAt={new Date()}
-                doneAt={null}/>
-                <Task desc="Comprar Livro" estimateAt={new Date()}
-                doneAt={new Date()}/>
-                <Task desc="Ler Livros" estimateAt={new Date()}
-                doneAt={null}/>
-                
+                    <FlatList data={this.state.tasks}
+                        keyExtractor={item => `${item.id}`}
+                        renderItem={({item}) => <Task {...item} toggleTask={this.toggleTask} />} />
                 </View>
             </View>
         )
     }
 }
 
-
 const styles = StyleSheet.create({
     container: {
         flex: 1
-    }, 
-    background: { 
+    },
+    background: {
         flex: 3
-    }, 
-    taskList: { flex: 7 }, 
-    titleBar: { flex: 1, 
-    justifyContent: 'flex-end',
-    }, title: { 
+    },
+    taskList: {
+        flex: 7
+    },
+    titleBar: {
+        flex: 1,
+        justifyContent: 'flex-end'
+    },
+    title: {
         fontFamily: commonStyles.fontFamily,
-        fontSize: 50, 
         color: commonStyles.colors.secondary,
-        marginLeft: 20, 
+        fontSize: 50,
+        marginLeft: 20,
         marginBottom: 20
-    }, 
-    subtitle:{
+    },
+    subtitle: {
         fontFamily: commonStyles.fontFamily,
-        fontSize: 20, 
         color: commonStyles.colors.secondary,
-        marginLeft: 20, 
-        marginBottom: 20
+        fontSize: 20,
+        marginLeft: 20,
+        marginBottom: 30
     }
-
-})
+});
