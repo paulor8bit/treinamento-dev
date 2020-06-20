2d550cfb3ca3b54abbb56891c41526c3cef74cb9
paulor8bit <thunder_momon@hotmail.com>
2020-06-20 17:14:58 -0300

    84. Modal Adicionar Tarefa #03

--- react-Native/tasksOffline/src/screens/AddTask.js
+++ react-Native/tasksOffline/src/screens/AddTask.js
@@ -1,49 +1,89 @@
-import React, {Component} from 'react'
-import { Modal, Text, View, StyleSheet, TouchableWithoutFeedback} from 'react-native'
-import commonStyles from '../commonStyles'
-export default class AddTask extends Component {
-    render() {
-        return  (
-       
-            <Modal transparent={true} visible={this.props.isVisible}
-        onRequestClose={this.props.onCancel}
-        animationType='slide'>
-            <TouchableWithoutFeedback
-                onPress={this.props.onCancel}> 
-                <View style={styles.casaa}></View>
-            </TouchableWithoutFeedback>
-           
-            <View style={styles.container}>
-            <Text style={styles.header}> Nova Tarefa</Text>
-            </View>
+import React, { Component } from 'react'
+import {
+    Modal,
+    View,
+    Text,
+    TouchableOpacity,
+    TextInput,
+    StyleSheet,
+    TouchableWithoutFeedback
+} from 'react-native'
 
-            <TouchableWithoutFeedback
-                onPress={this.props.onCancel}> 
-                <View style={styles.casaa}></View>
-            </TouchableWithoutFeedback>
-        </Modal>
-    )
+import commonStyles from '../commonStyles'
+
+const initialState = { desc: '' }
+
+export default class AddTask extends Component {
+
+    state = {
+        ...initialState
+    }
+
+    render() {
+        return (
+            <Modal transparent={true} visible={this.props.isVisible}
+                onRequestClose={this.props.onCancel}
+                animationType='slide'>
+                <TouchableWithoutFeedback
+                    onPress={this.props.onCancel}>
+                    <View style={styles.background}></View>
+                </TouchableWithoutFeedback>
+                <View style={styles.container}>
+                    <Text style={styles.header}>Nova Tarefa</Text>
+                    <TextInput style={styles.input} 
+                        placeholder="Informe a Descrição..."
+                        onChangeText={desc => this.setState({ desc })}
+                        value={this.state.desc} />
+                    <View style={styles.buttons}>
+                        <TouchableOpacity onPress={this.props.onCancel}>
+                            <Text style={styles.button}>Cancelar</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity>
+                            <Text style={styles.button}>Salvar</Text>
+                        </TouchableOpacity>
+                    </View>
+                </View>
+                <TouchableWithoutFeedback
+                    onPress={this.props.onCancel}>
+                    <View style={styles.background}></View>
+                </TouchableWithoutFeedback>
+            </Modal>
+        )
     }
 }
 
 const styles = StyleSheet.create({
-    casaa: {
-        flex: 1, 
+    background: {
+        flex: 1,
         backgroundColor: 'rgba(0, 0, 0, 0.7)'
-    }, 
+    },
     container: {
-        flex: 1, 
         backgroundColor: '#FFF'
-
-    }, 
-    header: { 
+    },
+    header: {
+        fontFamily: commonStyles.fontFamily,
         backgroundColor: commonStyles.colors.today,
         color: commonStyles.colors.secondary,
-        textAlign: 'center', 
-        padding: 15, 
+        textAlign: 'center',
+        padding: 15,
         fontSize: 18
-
+    },
+    input: {
+        fontFamily: commonStyles.fontFamily,
+        height: 40,
+        margin: 15,
+        backgroundColor: '#FFF',
+        borderWidth: 1,
+        borderColor: '#E3E3E3',
+        borderRadius: 6
+    },
+    buttons: {
+        flexDirection: 'row',
+        justifyContent: 'flex-end'
+    },
+    button: {
+        margin: 20,
+        marginRight: 30,
+        color: commonStyles.colors.today
     }
 })
-
-
