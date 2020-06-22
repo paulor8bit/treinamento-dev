bf87f7f3bf0e513fb1e0df6706374bd452f43bfb
paulor8bit <thunder_momon@hotmail.com>
2020-06-22 10:55:10 -0300

    Game Wizard

---
+++ p5Js/wizard/sketch.js
@@ -0,0 +1,14 @@
+let imagemCenario
+
+function preload() {
+  imageCenario = loadImage('imagens/cenraio/floresta.png')
+  
+}
+
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+}
