fc44f78493533afef389698fd56f27f754f9768e
paulor8bit <thunder_momon@hotmail.com>
2020-06-22 10:38:00 -0300

    92. Excluir Tarefa #02

--- react-Native/tasksOffline/src/components/Task.js
+++ react-Native/tasksOffline/src/components/Task.js
@@ -21,9 +21,20 @@ export default props => {
             </TouchableOpacity>
         )
     }
+
+    const getLeftContent = () => {
+        return (
+            <View style={styles.left}>
+                <Icon name="trash" size={20} color='#FFF' 
+                style={styles.excludeIcon}/>
+                <Text style={styles.excludeText}>Excluir</Text>
+            </View>
+        )
+    }
     return (
         <Swipeable
-        renderRightActions={getRightContent}>
+        renderRightActions={getRightContent}
+        renderLeftActions={getLeftContent}>
         <View style={styles.container}>
             <TouchableWithoutFeedback
             onPress={() => props.toggleTask(props.id)}>
@@ -60,7 +71,8 @@ const styles = StyleSheet.create({
         borderColor: '#AAA',
         borderBottomWidth: 1,
         alignItems: 'center',
-        paddingVertical: 10
+        paddingVertical: 10, 
+        backgroundColor: '#FFF'
     },
     checkContainer: {
         width: '20%',
@@ -98,5 +110,21 @@ const styles = StyleSheet.create({
         alignItems: 'center', 
         justifyContent: 'flex-end',
         paddingHorizontal: 20
+    }, 
+    left: { 
+        flex: 1,
+        backgroundColor: 'red', 
+        flexDirection: 'row', 
+        alignItems: 'center', 
+        justifyContent: 'flex-start',
+        paddingHorizontal: 20
+    }, excludeText: {
+        fontFamily: commonStyles.fontFamily, 
+        color: '#FFF', 
+        fontSize: 20, 
+        margin: 10
+    }, 
+    excludeIcon: { 
+        marginLeft: 10
     }
 })
