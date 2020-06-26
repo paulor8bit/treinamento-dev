1eadca31b4caa8385776f2222324c7884722b971
paulor8bit <thunder_momon@hotmail.com>
2020-06-26 09:26:09 -0300

    96. Middleware de Segurança

--- react-Native/tasksOnline/backend/index.js
+++ react-Native/tasksOnline/backend/index.js
@@ -4,6 +4,7 @@ const db = require('./config/db')
 const consign = require('consign')
 
 consign()
+    .include('./config/passport.js')
     .then('./config/middlewares.js')
     .then('./api')
     .then('./config/routes.js')
