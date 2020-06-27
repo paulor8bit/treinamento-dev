7532decd9b381570bd2db3b1cb5eed7a55424a73
paulor8bit <thunder_momon@hotmail.com>
2020-06-27 11:19:40 -0300

    98. API de Tarefas (Tasks) #02

--- react-Native/tasksOnline/backend/api/task.js
+++ react-Native/tasksOnline/backend/api/task.js
@@ -38,4 +38,31 @@ module.exports = app => {
         })
         .catch(err => res.status(400).json(err))
     }
+
+    const updateTaskDoneAt = (req, res, doneAt) =>{
+        app.db('task')
+            .where({id: req.params.id, userId: req.user.id})
+            .update({doneAt})
+            .then(_ => res.status(204).send())
+            .catch(err => res.status(400).json(err))
+    }
+
+    const toggleTask = (req, res) => { 
+        app.db('task')
+            .where({id: req.params, userId: req.user.id})
+            .first()
+            .then(task => {
+                if (!task) {
+                    const msg = `Task com id ${req.params.id} não encontrada`
+                    return res.status(400).send(msg)
+                }
+
+                const doneAt = task.doneAt ? null : new Date()
+                updateTaskDoneAt(req, res, doneAt)
+            })
+            .catch(err => res.status(400).json(err))
+    }
+
+    return {getTasks, save, remove, toggleTask}
+
 }
--- react-Native/tasksOnline/backend/config/routes.js
+++ react-Native/tasksOnline/backend/config/routes.js
@@ -1,4 +1,17 @@
 module.exports = app => { 
     app.post('/signup', app.api.user.save)
     app.post('/signin', app.api.auth.signin)
+
+    app.route('/task')
+        .all(app.config.passport.authenticate())
+        .get(app.api.task.getTasks)
+        .post(app.api.task.save)
+        
+        app.route('/task/:id')
+        .all(app.config.passport.authenticate())
+        .delete(app.api.task.remove)
+        
+        app.route('/tasks/:id/toogle/')
+        .all(app.config.passport.authenticate())
+        .put(app.api.task.toggleTask)
 }
