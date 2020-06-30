64cba4da79cc09e435f0c2237f3bb87c36c05018
paulor8bit <thunder_momon@hotmail.com>
2020-06-30 19:53:02 -0300

    124. Sair da Aplicação (Logout)

--- react-Native/tasksOnline/frontendMobile/src/screens/Menu.js
+++ react-Native/tasksOnline/frontendMobile/src/screens/Menu.js
@@ -1,53 +1,86 @@
 import React from 'react'
-import {ScrollView, View, Text, StyleSheet} from 'react-native'
-import {DrawerItems} from 'react-navigation-drawer'
-import {Gravatar} from 'react-native-gravatar'
+import { Platform, ScrollView, View, Text, StyleSheet, TouchableOpacity } from 'react-native'
+import { DrawerItems } from 'react-navigation-drawer'
+import { Gravatar } from 'react-native-gravatar'
+import commonStyles from '../commonStyles'
 
+import axios from 'axios'
+import { AsyncStorage } from 'react-native'
+import Icon from 'react-native-vector-icons/FontAwesome'
 
 export default props => {
+
+    const logout = () => {
+        delete axios.defaults.headers.common['Authorization']
+        AsyncStorage.removeItem('userData')
+        props.navigation.navigate('AuthOrApp')
+    }
 
     return (
         <ScrollView>
             <View style={styles.header}>
                 <Text style={styles.title}>Tasks</Text>
-                <Gravatar style={styles.avatar}
-                options={{
+                <Gravatar style={styles.avatar} 
+                    options={{
                         email: props.navigation.getParam('email'),
                         secure: true
-                }} />
-            <View style={styles.userInfo}> 
-                <Text style={styles.name}>{props.navigation.getParam('name')}</Text>
-                <Text style={styles.email}>{props.navigation.getParam('email')}</Text>
+                    }} />
+                <View style={styles.userInfo}>
+                    <Text style={styles.name}>
+                        {props.navigation.getParam('name')}
+                    </Text>
+                    <Text style={styles.email}>
+                        {props.navigation.getParam('email')}
+                    </Text>
+                </View>
+                <TouchableOpacity onPress={logout}>
+                    <View style={styles.logoutIcon}>
+                        <Icon name='sign-out' size={30} color='#800' />
+                    </View>
+                </TouchableOpacity>
             </View>
-            </View>
-            <DrawerItems {...props}/>
+            <DrawerItems {...props} />
         </ScrollView>
-    ) 
+    )
 }
- 
+
 const styles = StyleSheet.create({
     header: {
-        borderBottomWidth: 1, 
+        borderBottomWidth: 1,
         borderColor: '#DDD'
-    }, title: {
+    },
+    title: {
         color: '#000',
-        fontSize: 30, 
-        paddingTop: 30, 
+        fontFamily: commonStyles.fontFamily,
+        fontSize: 30,
+        paddingTop: Platform.OS === 'ios' ? 70 : 30,
         padding: 10
-    }, avatar: {
+    },
+    avatar: {
         width: 60,
         height: 60,
-        borderWidth: 3, 
-        borderRadius: 30, 
-        margin: 30
-    }, userInfo: {
-        marginLeft: 10, 
-    },name:{
-        fontSize: 20, 
-        marginBottom: 5
-        
-    }, email: {
-        fontSize: 15
+        borderWidth: 3,
+        borderRadius: 30,
+        margin: 10,
+        backgroundColor: '#222'
+    },
+    userInfo: {
+        marginLeft: 10,
+    },
+    name: {
+        fontFamily: commonStyles.fontFamily,
+        fontSize: 20,
+        color: commonStyles.colors.mainText,
+        marginBottom: 5,
+    },
+    email: {
+        fontFamily: commonStyles.fontFamily,
+        fontSize: 15,
+        color: commonStyles.colors.subText,
+        marginBottom: 10,
+    },
+    logoutIcon: {
+        marginLeft: 10,
+        marginBottom: 10
     }
-
 })
