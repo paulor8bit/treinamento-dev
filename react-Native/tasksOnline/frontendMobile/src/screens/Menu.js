0c324e68f1018f12f45a35d820d707ee8e9e351b
paulor8bit <thunder_momon@hotmail.com>
2020-06-30 15:58:04 -0300

    121. Informações de Usuário #02

--- react-Native/tasksOnline/frontendMobile/src/screens/Menu.js
+++ react-Native/tasksOnline/frontendMobile/src/screens/Menu.js
@@ -9,14 +9,15 @@ export default props => {
     return (
         <ScrollView>
             <View style={styles.header}>
+                <Text style={styles.title}>Tasks</Text>
                 <Gravatar style={styles.avatar}
                 options={{
                         email: props.navigation.getParam('email'),
                         secure: true
                 }} />
             <View style={styles.userInfo}> 
-                <Text>{props.navigation.getParam('name')}</Text>
-                <Text>{props.navigation.getParam('email')}</Text>
+                <Text style={styles.name}>{props.navigation.getParam('name')}</Text>
+                <Text style={styles.email}>{props.navigation.getParam('email')}</Text>
             </View>
             </View>
             <DrawerItems {...props}/>
@@ -28,12 +29,25 @@ const styles = StyleSheet.create({
     header: {
         borderBottomWidth: 1, 
         borderColor: '#DDD'
+    }, title: {
+        color: '#000',
+        fontSize: 30, 
+        paddingTop: 30, 
+        padding: 10
     }, avatar: {
         width: 60,
         height: 60,
         borderWidth: 3, 
         borderRadius: 30, 
         margin: 30
-    }, 
+    }, userInfo: {
+        marginLeft: 10, 
+    },name:{
+        fontSize: 20, 
+        marginBottom: 5
+        
+    }, email: {
+        fontSize: 15
+    }
 
 })
