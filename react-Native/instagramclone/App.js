962c425da24d37a427b56002529688e2ef450229
paulor8bit <thunder_momon@hotmail.com>
2020-07-01 18:51:19 -0300

    152. Componente Cabeçalho

--- react-Native/instagramclone/App.js
+++ react-Native/instagramclone/App.js
@@ -1,21 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+import React, {Component} from 'react';
+// import { StyleSheet, Text, View } from 'react-native';
+import Header from './src/components/Hearder'
+
+
+
+export default class App extends Component {
+  render() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Header />
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+}
