da19671eb7baaca082b0b54c09e8ba25b5fc2b10
paulor8bit <thunder_momon@hotmail.com>
2020-07-14 13:38:11 -0300

    185. Login com Redux

--- react-Native/instagramclone/index.js
+++ react-Native/instagramclone/index.js
@@ -1,9 +1,22 @@
+import React from 'react'
+import {Provider} from 'react-redux'
 import { registerRootComponent } from 'expo';
 
-import App from './App';
-import Feed from './src/screens/Feed'
+import {AppRegistry} from 'react-native'
 import Navigator from './src/Navigator'
+import {name as appName} from './app.json'
+import storeConfig from './src/store/storeConfig'
+
+const store = storeConfig()
+const Redux = () => (
+    <Provider store={store}>
+        <Navigator/>
+    </Provider>
+)
+
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in the Expo client or in a native build,
 // the environment is set up appropriately
-registerRootComponent(Navigator);
+
+// registerRootComponent(Navigator);
+AppRegistry.registerComponent(appName, () => Redux)
