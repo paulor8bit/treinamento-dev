e5e1f5e03fc7f842e811c3d7fa6cf448948567e3
paulor8bit <thunder_momon@hotmail.com>
2020-08-05 13:47:34 -0300

    InstagramClone

--- react-Native/instagramclone/index.js
+++ react-Native/instagramclone/index.js
@@ -1,22 +1,19 @@
 import React from 'react'
-import {Provider} from 'react-redux'
-import { registerRootComponent } from 'expo';
-
-import {AppRegistry} from 'react-native'
-import Navigator from './src/Navigator'
+import { Provider } from 'react-redux'
+import { AppRegistry } from 'react-native'
+import App from './src/App'
 import {name as appName} from './app.json'
+
 import storeConfig from './src/store/storeConfig'
+
+import axios from 'axios'
+axios.defaults.baseURL = 'https://lambe-bedee.firebaseio.com/'
 
 const store = storeConfig()
 const Redux = () => (
     <Provider store={store}>
-        <Navigator/>
+        <App />
     </Provider>
 )
 
-// registerRootComponent calls AppRegistry.registerComponent('main', () => App);
-// It also ensures that whether you load the app in the Expo client or in a native build,
-// the environment is set up appropriately
-
-// registerRootComponent(Navigator);
 AppRegistry.registerComponent(appName, () => Redux)
