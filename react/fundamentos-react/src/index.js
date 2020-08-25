a10daed2eedba4f4064e25103506f433e3bf2168
paulor8bit <thunder_momon@hotmail.com>
2020-08-25 09:42:31 -0300

    10. Carregando CSS

--- react/fundamentos-react/src/index.js
+++ react/fundamentos-react/src/index.js
@@ -1,5 +1,7 @@
 import ReactDOM from 'react-dom'
 import React from 'react'
+import './darkTheme.css'
+
 const tag = <div>Olá, <strong> Paulinho</strong>!</div>
 const el = document.getElementById('root')
 ReactDOM.render(<>{tag}</>, el)
