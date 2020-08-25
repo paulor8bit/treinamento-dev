a6dfaf99d05ef032f56a65734eb9be8400372c4c
paulor8bit <thunder_momon@hotmail.com>
2020-08-25 09:35:19 -0300

    9. Conhecendo JSX

--- react/fundamentos-react/src/index.js
+++ react/fundamentos-react/src/index.js
@@ -1,4 +1,5 @@
 import ReactDOM from 'react-dom'
-
+import React from 'react'
+const tag = <div>Olá, <strong> Paulinho</strong>!</div>
 const el = document.getElementById('root')
-ReactDOM.render('Olá Paulinho!', el)
+ReactDOM.render(<>{tag}</>, el)
