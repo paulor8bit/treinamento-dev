133227f9c1fa5479800ae119856ea30c5b796824
paulor8bit <thunder_momon@hotmail.com>
2020-08-27 11:55:57 -0300

    desafios 082720

--- desafios/eadbit/atual.js
+++ desafios/eadbit/atual.js
@@ -1,17 +1,24 @@
-// RemoveRepeatedLetters
-// Create a function that replaces all consecutively repeated letters in a word with single letters.
+// Triangle and Parallelogram Area Finder
+// Write a function that accepts base (decimal), height (decimal) and shape ("triangle", "parallelogram") as input and calculates the area of that shape.
 
 // Examples
-// removeRepeats("aaabbbccc") ➞ "abc"
+// areaShape(2, 3, "triangle") ➞ 3
 
-// removeRepeats("bookkeeper") ➞ "bokeper"
+// areaShape(8, 6, "parallelogram") ➞ 48
 
-// removeRepeats("nananana") ➞ "nananana"
+// areaShape(2.9, 1.3, "parallelogram") ➞ 3.77
 // Notes
-// N/A
+// Area of a triangle is 0.5 * b * h
+// Area of a parallelogram is b * h
+// Assume triangle and parallelogram are the only inputs for shape.
 
-function removeRepeats(str) {
-	return str.replace(/(.)\1+/gi,x=>x[0])
+function areaShape(base, height, shape) {
+	let tt = 0.5*base*height
+	let tp = base*height
+	return shape == "triangle" ? tt : tp
+	
 }
-console.log(removeRepeats("aaabbbccc"))
 
+console.log(areaShape(2, 3, "triangle") )
+console.log(areaShape(8, 6, "parallelogram"))
+console.log(areaShape(2.9, 1.3, "parallelogram"))
