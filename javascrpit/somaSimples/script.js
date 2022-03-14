82d6a9cdc7ac022911b718fa119606c066512c15
paulor8bit <thunder_momon@hotmail.com>
2022-03-14 15:57:38 -0300

    somaSimples

---
+++ javascrpit/somaSimples/script.js
@@ -0,0 +1,9 @@
+// a + b = soma
+
+let a = 5
+let b = 1
+let soma = a+b
+
+document.getElementById("a").innerHTML = `${a} +  `  
+document.getElementById("b").innerHTML = `${b}  =`  
+document.getElementById("soma").innerHTML = `${soma} `  
