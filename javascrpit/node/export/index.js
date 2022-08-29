9e3df6e99bc92a41eb9684a26848e359056441ef
paulor8bit <thunder_momon@hotmail.com>
2022-08-29 08:55:30 -0300

    node export

---
+++ javascrpit/node/export/index.js
@@ -0,0 +1,14 @@
+// Importing the func.js module
+
+// The ./ says that the func module
+// is in the same directory as
+// the main.js file
+const f = require('./modeloex');
+
+// Require returns an object with add()
+// and stores it in the f variable
+// which is used to invoke the required
+
+const result = f.add(10, 5);
+
+console.log('The result is:', result);
